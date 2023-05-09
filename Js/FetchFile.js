57c841b96de00942b68fc1737efd30af15068765
AsHansen88 <Ande970s@stud.kea.dk>
2023-05-09 14:29:23 +0200

    File list. Virker fra database

--- Js/FetchFile.js
+++ Js/FetchFile.js
@@ -33,10 +33,10 @@ function handleSubmit(event) {
 
 };
 
-//List
+//List and download
 
 // Fetch the file list from the server
-
+/*
 fetch('http://localhost:9090/files')
     .then(response => response.json())
     .then(data => {
@@ -51,7 +51,26 @@ fetch('http://localhost:9090/files')
         console.error('Error:', error);
     });
 
+*/
 
-//Download
+//List and download
 
+// Fetch the file list from the server
+fetch('http://localhost:9090/files')
+    .then(response => response.json())
+    .then(data => {
+        const fileListElement = document.getElementById('/files/');
+        data.forEach(file => {
+            const li = document.createElement('li');
+            const link = document.createElement('a');
+            link.textContent = file.name;
+            link.href = file.path; // Assuming the file path is provided by the server
+            link.download = file.name; // Set the filename for download
 
+            li.appendChild(link);
+            fileListElement.appendChild(li);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
